11349c7542eb92afd40d1f551340f4b9420d177f
Sudha8990AI <39717886+Sudha8990AI@users.noreply.github.com>
2018-06-09 17:05:44 -0700

    Commit
    Second Commit

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
 bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+    session.send('Sudhakar The AI said ' + session.message.text);
 });
